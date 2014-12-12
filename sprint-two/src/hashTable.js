ce83cfced6c98e2d7a73a79abd2b43186f69a1a7
agarza333 <adrianmgarza@gmail.com>
2014-12-12 11:47:10 -0800

    hashTable Pair

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,21 +3,54 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var newTuple = new Tuple(k,v);
+  var valCheck = this._storage.get(i);
+  if(!!valCheck){
+    valCheck.push(newTuple);
+    //console.log(valCheck);
+  }
+  else{
+  valCheck = [newTuple];
+  }
+  this._storage.set(i, valCheck);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var value = this._storage.get(i);
+  console.log(value);
+  if(value === undefined){
+    return false;
+  }
+  if(value.length === 1){
+    return value[0][1];
+  }
+  for(var i=0; i<value.length; i++){
+    if(value[i][0] === k){
+      return value[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var value = this._storage.get(i);
+  if(value.length === 1){
+    value[0][1] = null;
+  }
+  //console.log(value);
+  this._storage.set(i,value);
 
 };
 
 
+var Tuple = function(k, v){
 
+  return [k,v];
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
