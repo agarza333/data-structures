909befa08f56835112317fe597b09785224434ca
Adrian Garza <adrianmgarza@gmail.com>
2014-12-12 12:17:55 -0800

    hash, linkedlist, tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,49 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = new Node(value);
+    if(this.head === null){
+      this.head = newNode;   // this assignment gives us all properties of Node
+      this.tail = newNode;    // this assignment gives us all properties of Node
+    }else{
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    if(this.head !== null){
+      var oldHead = this.head;
+      this.head = this.head.next;
+      return oldHead.value;
+
+    }
   };
 
-  list.contains = function(target){
+  list.contains = function(target, currentNode){
+    var currentNode = currentNode || this.head;
+   
+      if (currentNode.value === target){
+        return true;
+      }
+      else if(currentNode.next === null){
+        return false;
+      }
+      else{
+        return this.contains(target, currentNode.next);
+      }
   };
 
   return list;
 };
+
+/*
+  lookup N
+  find N
+  append 1
+  insert 1
+  remove 1
+*/
 
 var Node = function(value){
   var node = {};
@@ -21,9 +54,13 @@ var Node = function(value){
   node.value = value;
   node.next = null;
 
+
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ /// addToTail = constant time
+ /// removeHead = constant time
+ /// contains = n
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -14,20 +14,22 @@ treeMethods.addChild = function(value){
 	this.children[this.getSize()] = newTree;
 };
 
+
 treeMethods.contains = function(target){
 	var child = this;
 	var traverseTrees = function(child){
-		console.log(child);
 		if(child.value === target){
 			return true;
 		}
 		else {
 			if(child.children){
-			console.log("object keys of children = ", Object.keys(child.children).length);
 			for(var i=0; i < Object.keys(child.children).length; i++){
-				return traverseTrees(child.children[i]); //need to be able to point from currentNode to first child
+				if(traverseTrees(child.children[i])){
+					return true; 
+				}//need to be able to point from currentNode to first child
 			}
 		}
+	}
 		return false;
 	};
 
@@ -38,8 +40,11 @@ treeMethods.getSize = function(){
 	
  return Object.keys(this.children).length; //gets the lenth of the object
 
-}
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ find N
+ insert 1
+ remove 1
  */
