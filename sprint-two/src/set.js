cf788b648906b332f5fe9e5f3cdc46d75e41b02a
Adrian Garza <adrianmgarza@gmail.com>
2014-12-12 15:25:29 -0800

    Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
+  
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+
+	this._storage[item] = item;
+
 };
 
 setPrototype.contains = function(item){
+
+	for(var k in this._storage){
+		if(this._storage[k] === item){
+			return true;
+		}
+	}
+	return false;
 };
 
 setPrototype.remove = function(item){
+	for(var k in this._storage){
+		if(this.contains(item)){
+			delete this._storage[k];
+		}
+	}
 };
 
 /*
